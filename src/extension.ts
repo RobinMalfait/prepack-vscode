6d10a063a475f13a84f9ecf1e55ac210c06ee113
Robin Malfait <malfait.robin@gmail.com>
2017-05-04 23:28:32 +0200

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -11,18 +11,9 @@ import {
     registerPrepackCommand,
     registerPrepackCommandOutput
 } from './commands';
-import EditProvider from './PrepackEditProvider';
-
-const VALID_LANG: DocumentSelector = ['javascript', 'javascriptreact'];
 
 export function activate(context: ExtensionContext) {
-    const editProvider = new EditProvider()
-
     const disposables = [
-        // Register all content providers
-        languages.registerDocumentRangeFormattingEditProvider(VALID_LANG, editProvider),
-        languages.registerDocumentFormattingEditProvider(VALID_LANG, editProvider),
-
         // Register all commands
         registerPrepackCommand(),
         registerPrepackCommandOutput(),
